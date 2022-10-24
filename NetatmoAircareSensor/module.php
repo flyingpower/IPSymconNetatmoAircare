45dd9b8c79cc427c92e764daf83c4e3a8b5e9a22
Ch. Damsky <demel42@web.de>
2022-10-24 12:15:37 +0200

    fix: parameter "expanded" of "ExpansionPanel" had trailing blank

--- NetatmoAircareSensor/module.php
+++ NetatmoAircareSensor/module.php
@@ -263,7 +263,7 @@ class NetatmoAircareSensor extends IPSModule
         $formActions[] = [
             'type'      => 'ExpansionPanel',
             'caption'   => 'Expert area',
-            'expanded ' => false,
+            'expanded'  => false,
             'items'     => [
                 $this->GetInstallVarProfilesFormItem(),
             ],
